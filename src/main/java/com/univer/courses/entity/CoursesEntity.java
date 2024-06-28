de9091d55fdcc25603e7026673f28e82f2362356
Dylan-Gutierrez <1232100579@univermilenium.edu.mx>
2024-06-27 20:05:33 -0600

    entregable dos

---
+++ src/main/java/com/univer/courses/entity/CoursesEntity.java
@@ -0,0 +1,45 @@
+
+package com.univer.courses.entity;
+import com.univer.courses.model.reponse.ClaseResponse;
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name= "CURSO")
+public class CoursesEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Id_clase")
+    private int id;
+    @Column(name = "NOMBRE")
+    private String name;
+    @Column (name="GRADO")
+    private Integer grado;
+    //private List<ClaseResponse> clase;
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Integer getGrado() {
+        return grado;
+    }
+
+    public void setGrado(Integer grado) {
+        this.grado = grado;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
